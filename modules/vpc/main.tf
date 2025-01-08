5998429b24b534861ea26fafbdb049140b518073
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-08 21:03:10 +0530

    create a peer connection

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,7 +15,8 @@ resource "aws_subnet" "subnet" {
   }
 }
 # peer connection between vpc's
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peerconn" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.vpc.id
+  auto_accept   = true
 }
